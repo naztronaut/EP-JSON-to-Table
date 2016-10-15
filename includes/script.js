ea5cd10c363e338431ed77d4712eb4d34fc4c48b
Nazmus <nazmus.nasir@gmail.com>
2016-10-15 12:36:11 -0400

    Better Error Handling

--- includes/script.js
+++ includes/script.js
@@ -8,37 +8,52 @@ Github: https://github.com/nlinux1/
 Custom JS file for JTable
 */
 
-$(document).ready(function () {
-    $("#myForm").on("submit", function (e) {
+$(document).ready(function() {
+    $("#myForm").on("submit", function(e) {
         $("#output").html(""); //clear HTML
-        var list = $("#text").val(); // read value from textarea 
-        jTable(list.toString()); // convert input to string
+        var list = $("#text").val(); //.split("\n"); // array item per newline 
+
+        jTable(list.toString());
         e.preventDefault();
     });
 });
 
-function jTable(data) {
-    var obj = JSON.parse(data); // parse the sent string back to JSON and store into obj
-    
-    // new table, thead and tr elements
-    var table = $("<table>").addClass("table table-striped table-hover table-bordered table-condensed table-responsive").attr("id", "myTable");
-    var thead = $("<thead>");
-    var row = $("<tr>");
-    for (var key in obj[0]) {
-        // add the column headers with the 'info' class - headers are the object keys - only goes through the first object in the array to get the key
-        $(table).append($(thead).append($(row).append($("<th>").addClass("info").append(key)))); 
-    }
-
-    // iterate through each object array
-    $(obj).each(function (i, e) {
-        var innerRow = $("<tr>"); //create tr element
-        for (var key in e) {
-            $(innerRow).append($("<td>").append(e[key])); //append value to each td element and append to tr above
+function jTable(obj) {
+    var objLen; // variable placeholder to store the number of records 
+    try {
+        obj = JSON.parse(obj);
+        var table = $("<table>").addClass("table table-striped table-hover table-bordered table-condensed table-responsive").attr("id", "myTable");
+        var thead = $("<thead>");
+        var row = $("<tr>");
+        $(table).append($(row).append($("<th>").addClass("info").append("#")));
+        //check to see if obj is in array format or basic JSON
+        if (obj[0] != undefined) {
+            for (var key in obj[0]) {
+                $(table).append($(thead).append($(row).append($("<th>").addClass("info").append(key))));
+            }
+            objLen = obj.length;
+        } else {
+            for (var key in obj) {
+                $(table).append($(thead).append($(row).append($("<th>").addClass("info").append(key))));
+            }
+            objLen = 1; //always will be one because it's just one JSON item
         }
-        $(table).append($(innerRow)); // append everything to the table created above
-    });
-    
-    //output results to #output and display number of records found in #rowCount
-    $("#output").append($(table));
-    $("#rowCount").text("Number of records:" + obj.length);
+
+        $(obj).each(function(i, e) {
+            var innerRow = $("<tr>");
+            $(innerRow).append($("<td>").append(i + 1));
+            for (var key in e) {
+                $(innerRow).append($("<td>").append(e[key]));
+            }
+            $(table).append($(innerRow));
+        });
+        $("#output").append($(table));
+
+        //    console.log(obj.length);
+        $("#rowCount").text("Number of records:" + objLen).show();
+
+    } catch (err) {
+        $("#output").append("<h3 class='text-danger container'>Could not process request, please check your input and try again!</h3>");
+        $("#output").append("<h5 class='bg-warning container'><strong>Error Logged: </strong>" + err + "</h5>");
+    }
 }
